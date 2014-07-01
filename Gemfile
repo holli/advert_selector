cedad3dd2de82ee3b777d19bec86e604fe468a85
Nikke <nileskel@gmail.com>
2014-07-01 14:21:05 +0300

    upgraded to rails 4.0.0

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem "jquery-rails"
 # To use debugger
 # gem 'ruby-debug'
 
+gem 'protected_attributes'
 gem 'simple_form'
 gem 'acts_as_list'
 #gem 'pry'
--- test/dummy/config/initializers/secret_token.rb
+++ test/dummy/config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Dummy::Application.config.secret_token = '42fb79fe9baf04a90a6caf05f98d4950b157d77f5ca4079104f4c5378607c705f60ac5421670f2384948a0860a6a5d7c41ce5e8015f7830c0d3290726d716808'
+Dummy::Application.config.secret_key_base = '7d5bb216d35a2613ff87254b811a64ccb8debcf172677f8111873da5b4b02b8169aba8fedf900cf21bf8d988914cf114d5848fbcf132484aef59f9a910d3d48c'
